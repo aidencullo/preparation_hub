16bbd95c18387d7e11bc5bb73abfd96b6283fbda
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 11:21:20 -0400

    n timespace passed sample testcases

--- balanceBrackets.py
+++ balanceBrackets.py
@@ -7,7 +7,22 @@ import math
 
 def isBalanced(s):
   # Write your code here
-  
+    stack = []
+    for c in s:
+        if c in '([{':
+            stack.append(c)
+            continue
+        if not stack:
+            return False
+        popped = stack.pop()
+        if c == ')' and popped != '(':
+            return False
+        if c == ']' and popped != '[':
+            return False
+        if c == '}' and popped != '{':
+            return False
+            
+    return not stack
 
 
 
